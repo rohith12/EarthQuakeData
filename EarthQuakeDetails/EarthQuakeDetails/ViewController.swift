1612582a6a5cac1a3d9a3a65ab79fca315435b7b
rohith <r12.rohith@gmail.com>
2021-04-01 15:56:26 -0500

    Initial setup

---
+++ EarthQuakeDetails/EarthQuakeDetails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EarthQuakeDetails
+//
+//  Created by Rohith Raju on 4/1/21.
+//  Copyright © 2021 Rohith Raju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
